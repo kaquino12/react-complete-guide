a62e36f85b77e11137a865f909c04f4c2bf21ce6
kaquino12 <kenneth.aquino@wondersco.com>
2022-03-11 19:41:00 -0500

    Made ExpenseItem component a little bit more complex

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
-function ExpenseItem () {
-    return <h2>Expense Item!</h2>
+function ExpenseItem() {
+    return (
+        <div>
+            <div>March 11th 2022</div>
+            <div>
+                <h2>Car Insurance</h2>
+                <div>P3000</div>
+            </div>
+        </div>
+    )
 }
 
 export default ExpenseItem;
